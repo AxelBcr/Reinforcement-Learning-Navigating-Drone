7efc65941618cdb2b89f4fa1466ed07ad48c2b4a
Warukho <axelbouchaudroche@gmail.com>
2024-12-06 17:23:38 +0100

    ChangingTarget.py needs some fixes on the training of the 2nd phase, commands or training are not well transmitted/done

--- ChangingTarget.py
+++ ChangingTarget.py
@@ -3,10 +3,10 @@ import sys
 from main import get_training_results, settings, writing_commands, createRoom, createDrone, DroneVirtual, np, settings, \
     training_loop
 
-# Initialisation des paramètres globaux
+# Inisialization of Best Episode Reward
 best_episode_reward = -float('inf')
 
-# %% Main code & Hyperparameters
+# %% Room creation & Hyperparameters & Commands writing
 room_description = f"(0 0, {settings["room_x"]-1} 0, {settings["room_x"]-1} {settings["room_y"]-1}, 0 {settings["room_y"]-1}, 0 0)"
 room = createRoom(room_description, settings["room_height"]-1)
 drone = createDrone("DroneVirtual", "ViewerTkMPL")
@@ -27,6 +27,7 @@ epsilon = 0.98  # Randomness rate
 epsilon_decay = 0.92  # Randomness decay rate
 epsilon_min = 0.01  # Minimum randomness rate
 
+# Get the results of the training, and write it to best_episode_commands.py
 best_episode_actions, best_episode_trajectory, settings = get_training_results(env_with_viewer)
 writing_commands(best_episode_actions, settings["room_x"], settings["room_y"], settings["room_height"],
                  settings["drone_x"], settings["drone_y"],
@@ -35,31 +36,27 @@ writing_commands(best_episode_actions, settings["room_x"], settings["room_y"], s
 print("Running updated best_episode_commands.py...")
 subprocess.run([sys.executable, "best_episode_commands.py"])
 
-def get_smoothed_commands(best_episode_actions):
-    smoothed_commands = []
-    for action in best_episode_actions:
-        direction, distance = action
-        smoothed_commands.append((direction, distance))
-    return smoothed_commands
-
-
 def main():
     from main import settings
 
+    #Gets  the last drone position
     last_drone_position = (settings["target_x"], settings["target_y"])
 
+    #Asks for the new target position
     new_target_position = (
         int(input("Enter the x coordinate of the new target: ")),
         int(input("Enter the y coordinate of the new target: ")),
         int(input("Enter the z coordinate of the new target: "))
     )
 
+    #Updates the settings
     settings["target_x"] = new_target_position[0]
     settings["target_y"] = new_target_position[1]
     settings["target_z"] = new_target_position[2]
     settings["drone_x"] = last_drone_position[0]
     settings["drone_y"] = last_drone_position[1]
 
+    #Runs the training with new positions, and updates the commands
     training_loop(env_with_viewer, settings["num_episodes"], settings["max_steps_per_episode"])
 
     best_episode_actions, best_episode_trajectory, settings = get_training_results(env_with_viewer)
